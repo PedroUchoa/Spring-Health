fad0ba564f572aa6884b9f7e0d96028b47970cf4
Pedro Uchoa <joaopedrouchoacampos@gmail.com>
2026-02-26 18:51:05 -0300

    Add Cache

--- src/main/java/com/pedro/health/service/DocumentService.java
+++ src/main/java/com/pedro/health/service/DocumentService.java
@@ -7,7 +7,11 @@ import com.pedro.health.infra.exceptions.document.DocumentNotFoundException;
 import com.pedro.health.infra.exceptions.person.PersonNotFoundException;
 import com.pedro.health.repositories.DocumentRepository;
 import com.pedro.health.repositories.PersonRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,11 +25,14 @@ public class DocumentService {
     @Autowired
     private PersonRepository personRepository;
 
+    @Cacheable(value = "document")
     public List<ReturnDocumentDto> returnAllDocumentsByPersonId(String personId){
         personRepository.findById(personId).orElseThrow(()->new PersonNotFoundException(personId));
         return documentRepository.findAllByPersonId(personId).stream().map(ReturnDocumentDto::new).toList();
     }
 
+    @Transactional
+    @CacheEvict(value = {"person","document"}, allEntries = true)
     public ReturnDocumentDto updateDocument(String documentId, CreateDocumentDto updateDocument){
         Document document = documentRepository.findById(documentId).orElseThrow(()-> new DocumentNotFoundException(documentId));
         document.update(updateDocument);
--- src/main/java/com/pedro/health/service/PersonService.java
+++ src/main/java/com/pedro/health/service/PersonService.java
@@ -6,7 +6,11 @@ import com.pedro.health.dtos.person.ReturnPersonDto;
 import com.pedro.health.dtos.person.UpdatePersonDto;
 import com.pedro.health.infra.exceptions.person.PersonNotFoundException;
 import com.pedro.health.repositories.PersonRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,16 +21,21 @@ public class PersonService {
     @Autowired
     private PersonRepository personRepository;
 
+    @Transactional
+    @CacheEvict(value = {"person","document"}, allEntries = true)
     public ReturnPersonDto createPerson(CreatePersonDto createPersonDto){
         Person person = new Person(createPersonDto);
         Person returnPerson = personRepository.save(person);
         return new ReturnPersonDto(returnPerson);
     }
 
+    @Cacheable(value = "person")
     public List<ReturnPersonDto> listAllPersons(){
         return personRepository.findAll().stream().map(ReturnPersonDto::new).toList();
     }
 
+    @Transactional
+    @CacheEvict(value = {"person","document"}, allEntries = true)
     public ReturnPersonDto updatePerson(String personId,UpdatePersonDto updatePerson){
         Person person = personRepository.findById(personId).orElseThrow(()->new PersonNotFoundException(personId));
         person.update(updatePerson);
@@ -34,6 +43,8 @@ public class PersonService {
         return new ReturnPersonDto(person);
     }
 
+    @Transactional
+    @CacheEvict(value = {"person","document"}, allEntries = true)
     public void deletePerson(String personId){
         Person person = personRepository.findById(personId).orElseThrow(()->new PersonNotFoundException(personId));
         person.disable();
